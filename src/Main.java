cf78ffdd9e752245152c483224fe87a3ece91d4c
Silvio <silviopinosastudillo@gmail.com>
2024-05-29 17:42:25 -0500

    Array ya implementado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        int[] Array = {42, 17, 23, 56, 89, 3, 14};
+
+        // Imprimir los valores del array
+        for (int i = 0; i < Array.length; i++) {
+            System.out.println("Elemento en el índice " + i + ": " + Array[i]);
+        }
     }
 }
---
+++ src/ObjectController.java
@@ -0,0 +1,24 @@
+public class ObjectController {
+    
+    // Método para ordenar el array usando el algoritmo de burbuja
+    public static void ordenarBurbuja(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Intercambiar array[j] y array[j + 1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void imprimirArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
+

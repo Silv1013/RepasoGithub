5ac6b857371ee3dec42364d6355efb697e5d3a83
Silvio <silviopinosastudillo@gmail.com>
2024-05-29 17:46:21 -0500

    [Appropriate message]

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        ObjectController metodo = new ObjectController();
         int[] Array = {42, 17, 23, 56, 89, 3, 14};
 
         // Imprimir los valores del array
